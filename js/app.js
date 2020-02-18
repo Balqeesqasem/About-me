659c5f760c3aa923f6585160ed5b22c1c4a34751
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-02-18 15:56:54 +0200

    edit js file

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ alert('welcome to my page ! Are you ready to know more about me?');
 var whoYouAre = prompt ('First can you told me what is your name');
 // console.log(whoYouAre);
 alert('Hello ' + whoYouAre + ' happy to talk with you!' + ' Lets start!');
-
+var counterForMarke = 0;
 var study = prompt (' Do you think that I studied photography at the university?').toUpperCase();
 // console.log(study);
 
@@ -14,7 +14,7 @@ if (study === 'YES' || study === 'Y' ) {
 else if (study === 'NO' || study === 'N')
 {
   alert(' No but i wish i did ! This is a great hobby for me, Photography is a beautiful passion , I hope you love to shoot and loves the beauty of pictures, especially nature pictures ');
-
+  counterForMarke = counterForMarke+1 ;
 }
 else {
   alert('You skip first qustion');
@@ -26,6 +26,7 @@ var love = prompt (' do you think that i love Reading ?').toUpperCase();
 
 if (love === 'YES' || love === 'Y') {
   alert('Oh thats right i love reading very much');
+  counterForMarke = counterForMarke+1 ;
 }
 
 else if (love === 'NO' || love ==='N')
@@ -48,7 +49,7 @@ if (book === 'YES' || book === 'Y') {
 else if (book === 'NO' || book === 'N')
 {
   alert(' Yes, the novels are great, but you have to choose them carefully to be as good as you want. There will be some novels that I have read and liked.');
-
+  counterForMarke = counterForMarke+1 ;
 }
 else {
   alert('You skip third qustion');
@@ -65,7 +66,7 @@ if (grade === 'YES' || grade === 'Y') {
 else if (grade === 'NO' || grade === 'N')
 {
   alert(' right. that was hard!');
-
+  counterForMarke = counterForMarke+1 ;
 }
 else {
   alert('You skip fourth qustion');
@@ -78,16 +79,46 @@ var film= prompt (' do you think that i love Arabic movies more than English?').
 
 if (film === 'YES' || film === 'Y') {
   alert('It is hard to find beautiful Arabic movies, so I dont like them more than english movies The best Arabic movies comedies movies Like Ahmed Helmys films');
-
+  counterForMarke = counterForMarke+1 ;
 }
 
 else if (film === 'NO' || film === 'N')
 {
   alert('Not all movies, but there are really great movies');
-
+  counterForMarke = counterForMarke+1 ;
 }
 else {
   alert('You skip fifth qustion' );
 }
 
+for (var i = 0; i< 4; i++) {
+  var MyCollegeYears = parseInt (prompt('Expect how many years of my life I spent at university'));
+  if (MyCollegeYears === 5){
+    alert('It is right');
+    i=i+10;
+    counterForMarke = counterForMarke+1 ;
+  }
+  else if(MyCollegeYears > 5){
+    alert('Too high');
+  } else if (MyCollegeYears < 5)
+  {alert('too low');}else{
+    alert('Some thing rong');
+  }
+  if (i===3){
+    alert('I spent 5 years');
+  }
+}
+
 alert ('It was a beautiful time, wasnt it? Thank you for your time ' + whoYouAre );
+alert('Your scoor is : ' + counterForMarke + '/7');
+
+
+
+
+
+// var MyCollegeYears = parseInt (prompt('Expect how many years of my life I spent at university'));
+// while ()
+// { var MyCollegeYears = parseInt (prompt('Expect how many years of my life I spent at university'));
+
+// }
+
